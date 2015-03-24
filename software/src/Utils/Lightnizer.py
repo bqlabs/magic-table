37c29ea85162bbd00ca964f0af4f73a13af1a09b
David-Estevez <david.estevez.fdez@gmail.com>
2015-03-24 12:00:35 +0100

    Add support for different colors

--- software/src/Utils/Lightnizer.py
+++ software/src/Utils/Lightnizer.py
@@ -1,9 +1,16 @@
 
 __author__ = 'def'
 
-gcode_file = '/home/def/Desktop/gcodes/bq_logo.gcode'
+#gcode_file = '/home/def/Desktop/gcodes/bq_logo.gcode'
+gcode_file = '../gear.gcode'
+
 turn_off_cmd = "M42 P6 S0 ;turn off led\nM42 P5 S0 ;turn off led\nM42 P4 S0 ;turn off led\n"
 turn_on_cmd = "M42 P6 S255 ;turn on led\nM42 P5 S255 ;turn on led\nM42 P4 S255 ;turn on led\n"
+
+green_led_cmd = "M42 P6 S0 ;turn off led\nM42 P5 S5 ;turn on green led\nM42 P4 S0 ;turn off led\n"
+red_led_cmd = "M42 P6 S0 ;turn off led\nM42 P5 S0 ;turn off led\nM42 P4 S5 ;turn on red led\n"
+blue_led_cmd = "M42 P6 S5 ;turn on blue led\nM42 P5 S0 ;turn off led\nM42 P4 S0 ;turn off led\n"
+
 start_cmd =   ["G21        ;metric values\n",
                "G90        ;absolute positioning\n",
                "M107       ;start with the fan off\n",
@@ -20,13 +27,26 @@ def main():
         gcode = f.readlines()
 
     for line in gcode:
-        if line.find('Z') != -1:
-            if led_on:
-                new_gcode.append(turn_off_cmd)
-                led_on = False
+        z_start = line.find('Z')
+        if z_start != -1:
+            z_end = line.find('F')
+            if z_end != -1:
+                value = float(line[z_start+1:z_end])
             else:
+                value = float(line[z_start+1:])
+
+            if value < -3:
+                new_gcode.append(blue_led_cmd)
+            elif value < -2:
+                new_gcode.append(green_led_cmd)
+            elif value < -1:
+                new_gcode.append(red_led_cmd)
+            elif value < 0:
+                # new_gcode.append('; lights on\n')
                 new_gcode.append(turn_on_cmd)
-                led_off = True
+            else:
+                # new_gcode.append('; lights on\n')
+                new_gcode.append(turn_off_cmd)
         else:
             new_gcode.append(line)
 
