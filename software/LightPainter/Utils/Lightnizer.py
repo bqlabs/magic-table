0c3243444beddc161b1298ac45a10dac9bd1cef8
David-Estevez <david.estevez.fdez@gmail.com>
2015-04-15 16:49:46 +0200

    Convert Lightnizer to standalone app

--- software/LightPainter/Utils/Lightnizer.py
+++ software/LightPainter/Utils/Lightnizer.py
@@ -1,15 +1,16 @@
+import optparse
 
 __author__ = 'def'
 
 #gcode_file = '/home/def/Desktop/gcodes/bq_logo.gcode'
 gcode_file = '../gear.gcode'
 
-turn_off_cmd = "M42 P6 S0 ;turn off led\nM42 P5 S0 ;turn off led\nM42 P4 S0 ;turn off led\n"
-turn_on_cmd = "M42 P6 S255 ;turn on led\nM42 P5 S255 ;turn on led\nM42 P4 S255 ;turn on led\n"
+turn_off_cmd = "M42 P%d S0 ;turn off led\nM42 P%d S0 ;turn off led\nM42 P%d S0 ;turn off led\n"
+turn_on_cmd = "M42 P%d S255 ;turn on led\nM42 P%d S255 ;turn on led\nM42 P%d S255 ;turn on led\n"
 
-green_led_cmd = "M42 P6 S0 ;turn off led\nM42 P5 S5 ;turn on green led\nM42 P4 S0 ;turn off led\n"
-red_led_cmd = "M42 P6 S0 ;turn off led\nM42 P5 S0 ;turn off led\nM42 P4 S5 ;turn on red led\n"
-blue_led_cmd = "M42 P6 S5 ;turn on blue led\nM42 P5 S0 ;turn off led\nM42 P4 S0 ;turn off led\n"
+red_led_cmd = "M42 P%d S5 ;turn on red led\nM42 P%d S0 ;turn off led\nM42 P%d S0 ;turn off led\n"
+green_led_cmd = "M42 P%d S0 ;turn off led\nM42 P%d S5 ;turn on green led\nM42 P%d S0 ;turn off led\n"
+blue_led_cmd = "M42 P%d S0 ;turn off led\nM42 P%d S0 ;turn off led\nM42 P%d S5 ;turn on blue led\n"
 
 start_cmd =   ["G21        ;metric values\n",
                "G90        ;absolute positioning\n",
@@ -17,13 +18,13 @@ start_cmd =   ["G21        ;metric values\n",
                "G28 Y0     ;move Y to min endstops\n",
                "G28 X0     ;move X to min endstops\n"]
 
-def main():
+def process_gcode(input_file, output_file, red_pin, green_pin, blue_pin):
     gcode = []
     new_gcode = []
 
     led_on = True
 
-    with open(gcode_file, 'r') as f:
+    with open(input_file, 'r') as f:
         gcode = f.readlines()
 
     for line in gcode:
@@ -36,22 +37,46 @@ def main():
                 value = float(line[z_start+1:])
 
             if value < -3:
-                new_gcode.append(blue_led_cmd)
+                new_gcode.append(blue_led_cmd % (red_pin, green_pin, blue_pin))
             elif value < -2:
-                new_gcode.append(green_led_cmd)
+                new_gcode.append(green_led_cmd % (red_pin, green_pin, blue_pin))
             elif value < -1:
-                new_gcode.append(red_led_cmd)
+                new_gcode.append(red_led_cmd % (red_pin, green_pin, blue_pin))
             elif value < 0:
                 # new_gcode.append('; lights on\n')
-                new_gcode.append(turn_on_cmd)
+                new_gcode.append(turn_on_cmd % (red_pin, green_pin, blue_pin))
             else:
                 # new_gcode.append('; lights on\n')
-                new_gcode.append(turn_off_cmd)
+                new_gcode.append(turn_off_cmd % (red_pin, green_pin, blue_pin))
         else:
             new_gcode.append(line)
 
-    with open(gcode_file+'_mod.gcode','w') as f:
+    with open(output_file,'w') as f:
         f.writelines(start_cmd + new_gcode[2:] + start_cmd)
 
 if __name__ == '__main__':
-    main()
+
+    # Parse command-line parameters
+    parser = optparse.OptionParser("usage: %prog [options]")
+    parser.add_option("-i", "--input", dest="input_file", default="", type="string",help="Input gcode")
+    parser.add_option("-o", "--output", dest="output_file", default="", type="string",help="Output file in which the result will be saved")
+    parser.add_option("-r", "--red", dest="red_pin", default=4, type="int",help="Pin for the red LED")
+    parser.add_option("-g", "--green", dest="green_pin", default=5, type="int",help="Pin for the green LED")
+    parser.add_option("-b", "--blue", dest="blue_pin", default=6, type="int",help="Pin for the blue LED")
+    (options, args) = parser.parse_args()
+
+    red_pin = options.red_pin
+    green_pin = options.green_pin
+    blue_pin = options.blue_pin
+
+    output_file = options.output_file
+    input_file = options.input_file
+
+    if not input_file:
+        print "No input file was specified"
+        exit(1)
+
+    if not output_file:
+        output_file = input_file+'_mod.gcode'
+
+    process_gcode(input_file, output_file, red_pin, green_pin, blue_pin)
