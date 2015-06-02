4b9c3e8c6e7d34b1f91e78dbdca337e7c90997f2
David-Estevez <david.estevez.fdez@gmail.com>
2015-06-02 17:24:38 +0200

    Add files for image stitching when using the microscope scanner

---
+++ software/Microscope Scanning/src/StitchingImages.py
@@ -0,0 +1,13 @@
+import cv2
+import numpy as np
+
+__author__ = 'def'
+
+def main():
+    image1 = cv2.imread("../images/scan01.jpg")
+    image2 = cv2.imread("../images/scan02.jpg")
+
+    stitcher = cv2.
+
+if __name__ == '__main__':
+    main()
